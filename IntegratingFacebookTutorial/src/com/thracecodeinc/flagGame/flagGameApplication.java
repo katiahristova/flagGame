9b9567b4cac4da38436943f42500eb5ef8d830ac
Sami <YOUR EMAIL ADDRESSsmail.ali@mail.mail.citytech,cuny.edu>
2015-05-28 18:14:55 -0400

    push notif. not working yet

--- IntegratingFacebookTutorial/src/com/thracecodeinc/flagGame/flagGameApplication.java
+++ IntegratingFacebookTutorial/src/com/thracecodeinc/flagGame/flagGameApplication.java
@@ -1,9 +1,15 @@
 package com.thracecodeinc.flagGame;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.PushService;
+import com.parse.SaveCallback;
 import com.thracecodeinc.flagGame.R;
 
 public class flagGameApplication extends Application {
@@ -15,6 +21,20 @@ public class flagGameApplication extends Application {
     super.onCreate();
       Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_id));
       ParseFacebookUtils.initialize(getApplicationContext());
+      ParseInstallation.getCurrentInstallation().saveInBackground();
+
+      ParsePush.subscribeInBackground("ChallengePush", new SaveCallback() {
+        @Override
+        public void done(ParseException e) {
+          if (e == null) {
+            Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+          } else {
+            Log.e("com.parse.push", "failed to subscribe for push", e);
+          }
+        }
+      });
+
+      PushService.setDefaultPushCallback(this, OfflineGame.class);
   }
 
 
